2af5735252838d9f236bfaec76ebf41d158271ec
Adrian Mos <adrian.raul.mos@gmail.com>
2018-02-22 22:55:00 +0200

    cleanup

--- hashing/hashing-division-method/hashing-division-method.c
+++ hashing/hashing-division-method/hashing-division-method.c
@@ -25,21 +25,18 @@ const char LENGTH = 17;
 
 void print_array(char *pArray) {
 	printf("[ ");
-	for (char i=0; i<LENGTH; i++) {
-		printf("%2d ", *(pArray+i));
+	for (char i = 0; i<LENGTH; i++) {
+		printf("%2d ", *(pArray + i));
 	}
 	printf("]");
 }
 
 void print_line_break() {
-	printf("------------------------------------------------------------------------\n");
+	printf("\n---------------------------------------------------------------------------\n");
 }
 
-void case1() {
-	int m = 12;
-	char subset[LENGTH] = { 0, 2, 4, 6,  8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32 };
-	
-	printf("\nsubset includes only odd numbers; m=%d is a multiple of d(=2); h(k)=k mod m\n", m);
+void calculate_and_print_hashes(char subset[], int m) {
+
 	printf("\nsubset= ");
 	print_array(&subset[0]);
 
@@ -48,72 +45,61 @@ void case1() {
 		printf("%2d ", hash(subset[i], m));
 	}
 	printf("] \n");
-	print_line_break();
 }
-
-void case2() {
-	int m = 12;
-	char subset[LENGTH] = { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48 };
-
-	printf("\nsubset includes divisors of 3; m=%d is a multiple of d; h(k)=k mod m\n", m);
-	printf("\nsubset= ");
-	print_array(&subset[0]);
-
-	printf("\nhashed= [ ");
-	for (int i = 0; i < LENGTH; i++) {
-		printf("%2d ", hash(subset[i], m));
-	}
-	printf("] \n");
-	print_line_break();
-}
-
-void case3() {
-	int m = 13;
-	char subset[LENGTH] = { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48 };
-
-	printf("\nsubset includes divisors of 3; m=%d is a prime number; h(k)=k mod m\n", m);
-	printf("\nsubset= ");
-	print_array(&subset[0]);
-
-	printf("\nhashed= [ ");
-	for (int i = 0; i < LENGTH; i++) {
-		printf("%2d ", hash(subset[i], m));
-	}
-	printf("] \n");
-	print_line_break();
-}
-
-void case4() {
-	int m = 13;
-	char subset[LENGTH] = { 0, 2, 4, 6,  8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32 };
-
-	printf("\nsubset includes only odd numbers; m=%d is a prime number; h(k)=k mod m\n", m);
-	printf("\nsubset= ");
-	print_array(&subset[0]);
-
-	printf("\nhashed= [ ");
-	for (int i = 0; i < LENGTH; i++) {
-		printf("%2d ", hash(subset[i], m));
-	}
-	printf("] \n");
-	print_line_break();
-}
-
 
 int main()
 {
-	case1();
+	int m;
+
+	m = 12;
+	char subset[LENGTH] = { 0, 2, 4, 6,  8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32 };
+	printf("\nsubset includes only odd numbers; m=%d is a multiple of d(=2); h(k)=k mod m\n", m);
+	
+	calculate_and_print_hashes(&subset[0], m);
 
 	printf("\n=> CONCLUSION: - bad distribution when k and m have common factors;");
 	printf("\n                 in this case the even slots are not used at all");
-	printf("\n\n\n");
+	print_line_break();
+	printf("\n");
 
-	case4();
+	//-----------------------------------------
+
+	m = 13;
+	char subset2[LENGTH] = { 0, 2, 4, 6,  8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32 };
+	printf("\nsubset includes only odd numbers; m=%d is a prime number; h(k)=k mod m\n", m);
+
+	calculate_and_print_hashes(&subset2[0], m);
+
 	printf("\n=> CONCLUSION: - good distriution when m is prime");
-	printf("\n\n\n");
+	print_line_break();
+	printf("\n");
 
-	case2();
-	case3();
+	//-----------------------------------------
+
+
+	m = 12;
+	char subset3[LENGTH] = { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48 };
+	printf("\nsubset includes divisors of 3; m=%d is a multiple of d; h(k)=k mod m\n", m);
+
+	calculate_and_print_hashes(&subset3[0], m);
+
+	printf("\n=> CONCLUSION: - very bad distribution, only 4 slots used");
+	print_line_break();
+	printf("\n");
+
+	//-----------------------------------------
+
+
+	m = 13;
+	char subset4[LENGTH] = { 0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48 };
+	printf("\nsubset includes divisors of 3; m=%d is a prime number; h(k)=k mod m\n", m);
+
+	calculate_and_print_hashes(&subset4[0], m);
+
+	printf("\n=> CONCLUSION: - prime number saves us");
+	print_line_break();
+	printf("\n");
+
 
 	getchar();
 	return 0;
